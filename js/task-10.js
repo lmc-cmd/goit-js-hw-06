aa68f03cc6aeee59de598eee5aace442feeb2cfa
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-06-26 18:47:00 +0300

    upd 10task

--- js/task-10.js
+++ js/task-10.js
@@ -1,55 +1,31 @@
-// "use strict";
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+"use strict";
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-// const inputEl = document.querySelector("[type=number]");
-// const btnCreateEl = document.querySelector("[data-create");
-// const btnDestroyEl = document.querySelector("[data-destroy");
-// const divBoxEl = document.querySelector("#boxes");
+const inputEl = document.querySelector("[type=number]");
+const btnCreateEl = document.querySelector("[data-create");
+const btnDestroyEl = document.querySelector("[data-destroy");
+const divBoxEl = document.querySelector("#boxes");
 
-// console.log(inputEl.value);
+btnCreateEl.addEventListener("click", onBtnClick);
 
-// // function onBtnClick(e, value) {
-// //   document.createElement(`div`);
-// //   // console.log(inputEl.value);
-// // }
-// // let arr = [];
-// btnCreateEl.addEventListener("click", onBtnClick);
+btnDestroyEl.addEventListener("click", (e) => {
+  document.location.reload();
+});
+console.log(inputEl.value);
 
-// function mostNumbers() {
-//   let arr = [];
-//   for (let i = 0; i <= inputEl.value; i++) {
-//     arr.push(i);
-//     console.log(arr);
-//   }
-//   return arr;
-// }
-// function onBtnClick(e) {
-//   const makeDivEl = (el) => {
-//     const divEl = document.createElement(`div`);
-//     divEl.style.width = `30px`;
-//     divEl.style.height = `30px`;
-//     divEl.style.backgroundColor = getRandomHexColor();
-//     return divEl;
-//   };
+function onBtnClick(e) {
+  const makeDivEl = (el) => {
+    const divEl = document.createElement(`div`);
+    divEl.style.width = divBoxEl.children.length + 30 + `px`;
+    divEl.style.height = divBoxEl.children.length + 30 + `px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    console.dir(divEl.style.width);
+    return divEl;
+  };
 
-//   for (let i of arr) {
-//     divBoxEl.append(makeDivEl());
-//     // }= 0; i < inputEl.value; i++) {
-//     //   divBoxEl.append(makeDivEl());
-//     // }
-//   }
-// }
-// // 1. взять inputEl.value из  этого числа сделать масив от 1 до значения инпута
-// // 2. вешаем слушатель на кнопку при клике создать {пишем функицю на создание элемента
-// //   задаем стили цвет= функцияРандом, а размеры 30px}
-// // 3. пишем функцию создания дивов по масиву.map (e) и размеру каждого el добавляем индекс или значение к px
-// // function mostNumbers() {
-// //   var arr = [];
-// //   for (var i = 0; i < arguments.length; i++) {
-// //     arr.push(arguments[i]);
-// //   }
-// //   return arr;
-// // }
-// // console.log(mostNumbers(1, 2, 3));
+  for (let i = 0; i < inputEl.value; i++) {
+    divBoxEl.append(makeDivEl());
+  }
+}
